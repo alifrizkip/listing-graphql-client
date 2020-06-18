7afca4dfecc5649cb21c975feedd04eec544c264
alip <alifrizkipambudi@gmail.com>
2020-06-18 19:58:18 +0700

    query and mutation to graphql api

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+import { Listings } from "./sections";
+
+render(
   <React.StrictMode>
-    <App />
+    <Listings title="TinyHouse Listings Props" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
